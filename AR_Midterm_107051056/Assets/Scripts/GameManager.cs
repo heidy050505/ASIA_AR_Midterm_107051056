0377976a837e35d8f42fc01ed76979e3e609a07f
heidy050505 <heidy050505@gmail.com>
2020-11-08 19:21:42 +0800

    新增遊戲控制器欄位

---
+++ AR_Midterm_107051056/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("石頭蛙")]
+    public Transform StoneFrog;
+    
+    [Header("太空人")]
+    public Transform Astronaut;
+
+    private void Update()
+    {
+        
+    }
+}
