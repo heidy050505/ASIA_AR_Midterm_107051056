3fdc9c4a071d1994524ea904c02a62fc8a7bc733
heidy050505 <heidy050505@gmail.com>
2020-11-10 22:49:11 +0800

    跑步跳攻擊按鈕設定完成

--- AR_Midterm_107051056/Assets/Scripts/GameManager.cs
+++ AR_Midterm_107051056/Assets/Scripts/GameManager.cs
@@ -8,8 +8,19 @@ public class GameManager : MonoBehaviour
     [Header("太空人")]
     public Transform Astronaut;
 
+    [Header("石頭蛙控制元件")]
+    public Animator AniStoneFrog;
+    [Header("太空人控制元件")]
+    public Animator AniAstronaut;
+
     private void Update()
     {
         
+    }
+
+    public void PlayAnimation(string aniname)
+    {
+        AniStoneFrog.SetTrigger(aniname);
+        AniAstronaut.SetTrigger(aniname);
     }
 }
